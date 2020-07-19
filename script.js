5a44adf834dd2d1fbf963b1fab145135db20dbea
Siksinats <67030519+Siksinats@users.noreply.github.com>
2020-07-19 16:31:07 -0400

    Added search by ingredient functionality

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ if (document.readyState == 'loading') {
 function ready() {
     /*--Display the entire recipe list at start--*/
     fullDisplay();
+    
 
 
     /*--Test the value of the box by btn--*/
@@ -15,8 +16,8 @@ function ready() {
     
     /*--Test the value of the box by typing--*/
     var mealInp = document.querySelector('#description-input')
-    mealInp = mealInp.values
-    mealInp = addEventListener("keydown", getMeal)
+    mealInp.addEventListener("keydown", getMeal)
+    
 }
 
 function getMeal() { 
@@ -30,38 +31,21 @@ function getMeal() {
         /*--Check readyState and init variables--*/
         if (meals.readyState === 4 && meals.status == 200) {
             let mealInfo = JSON.parse(meals.responseText);
-            let mealInput = document.querySelector("input").value;
-            let ingredientsHTML = '<div class="ingredients">';
-            let mealList = '';
+            let mealInput = document.querySelector("#description-input").value;
 
             /* Search for meal by name */
-                sort(mealInfo, mealInput, 'name')
-               
+                sort(mealInfo, mealInput, 'name', '.name-list')
+            
+            /* Search for meal by type */
+                sort(mealInfo, mealInput, 'type', '.type-list')
 
-                /* Search for meal by type */
-                for(let i=0; i<mealInfo.length; i++) {
+            /* Search for meal by ingredient */
+                sort(mealInfo, mealInput, 'ingredients', '.ingredient-list')
 
-                    if(mealInfo[i].type.toLocaleLowerCase() === mealInput.toLocaleLowerCase()) {
-                        mealList += `<div class="grow fade-in shadow card m-2 mb-3" style="width: 18rem;">
-                        <img src="${listInfo[i].picture}" class="card-img-top card-img-height img-thumbnail " alt="...">
-                        <div class="card-body">
-                          <h5 class="card-title">${listInfo[i].name}</h5>
-                          <p class="card-text">${listInfo[i].description}</p>
-                        </div>
-                      </div>`;
-                        document.querySelector('.full-list').style.display = 'none';
-                        
-                    } 
-                }
-
-                /*--Write HTML and close <div>--*/
-                document.querySelector('.meal_list').innerHTML = mealList;
-                
-                console.log(document.querySelector('.ingredient_list').innerHTML)
-
-                /*--Reset fullList display if no changes were made--*/
-                if(document.querySelector('.meal_list').innerHTML === '' &&
-                document.querySelector('.ingredient_list').innerHTML === '') {
+            /*--Reset fullList display if no changes were made--*/
+                if(document.querySelector('.type-list').innerHTML === '' &&
+                document.querySelector('.name-list').innerHTML === '' &&
+                document.querySelector('.ingredient-list').innerHTML === '') {
                         
                     document.querySelector('.full-list').style.display = 'flex';
                     
@@ -69,7 +53,6 @@ function getMeal() {
         }     
     };
 }
-  
 
 function fullDisplay() {
     var fullList = new XMLHttpRequest();
@@ -81,44 +64,62 @@ function fullDisplay() {
             listInfo = JSON.parse(fullList.responseText);
             
             for(let i=0; i<listInfo.length; i++) {
-                listDisplay += `<div class="grow fade-in shadow card m-2 mb-3" style="width: 18rem;">
-                <img src="${listInfo[i].picture}" class="card-img-top card-img-height img-thumbnail " alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">${listInfo[i].name}</h5>
-                  <p class="card-text">${listInfo[i].description}</p>
+                listDisplay += ` <a class="" href="" data-toggle="modal" data-target="#recipeModal">
+                <div id="mealCard" class="grow shadow card m-2 mb-3" style="width: 18rem;">
+                    <img src="${listInfo[i].picture}" class="card-img-top card-img-height" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">${listInfo[i].name}</h5>
+                    <p class="card-text">${listInfo[i].description}</p>
+                    </div>
                 </div>
-              </div>
-              `
+              </a> `
             }
             document.querySelector('.full-list').innerHTML = listDisplay
         }
     };
 }
 
-function sort(list, input, filter) {
+function sort(list, input, filter, listType) {
     let mealList = ''
-    for(let i=0; i<list.length; i++) {
+    let breakStatus = false;
 
-        if(list[i][filter].toLowerCase() === input.toLowerCase()) {
-            mealList += `<div class="grow fade-in shadow card m-2 mb-3" style="width: 18rem;">
-            <img src="${listInfo[i].picture}" class="card-img-top card-img-height img-thumbnail " alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${listInfo[i].name}</h5>
-              <p class="card-text">${listInfo[i].description}</p>
-            </div>
-          </div>`;
-            document.querySelector('.full-list').style.display = 'none';
-            console.log(mealList)
+    for(let i=0; i<list.length; i++) {
+        for(let j=0; j<list[i][filter].length; j++) {
+            if(list[i][filter][j].toLowerCase() == input.toLowerCase().trim()) {
+                
+                
+                
+                mealList += `<div id="mealCard" class="fade-in grow shadow card m-2 mb-3" style="width: 18rem;">
+                <img src="${listInfo[i].picture}" class="card-img-top card-img-height img-thumbnail " alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${listInfo[i].name}</h5>
+                <p class="card-text">${listInfo[i].description}</p>
+                </div>
+                </div>`;
+                document.querySelector('.full-list').style.display = 'none';                
+            
+
+                if(input.length > list[i][filter][j].length) {
+                    breakStatus = true;
+                    break;
+                }
+            }
+        
+            
         }
-        console.log(i)
+        if(breakStatus == true) {
+            break;
+        }
+        
         if(i+1 == list.length) {
-            console.log(mealList)
-            document.querySelector('.ingredient_list').innerHTML = mealList; 
-            console.log(document.querySelector('.ingredient_list').innerHTML)
-        }
+            document.querySelector(listType).innerHTML = mealList; 
+            
+        } 
     }   
  
 }
+
+
 
 
 
